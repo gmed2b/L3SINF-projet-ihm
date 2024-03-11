7355a62e3f5557c23696b5ad60d62d8ce1f72f4e
antocreadev <anthonymenghi@protonmail.com>
2024-03-11 13:11:43 +0100

    init commit of api branch

--- api/main.py
+++ api/main.py
@@ -1,28 +1,46 @@
-from fastapi import FastAPI, HTTPException, Depends
-from datetime import datetime
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm, HTTPBearer
+from jose import JWTError, jwt 
+from datetime import datetime, timedelta, timezone
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
-import tasks
 import schemas
+import tasks
 
-# Catégories des endpoints (voir documentations Swagger/redocs)
-tags_metadata = [
-     {
-        "name": "Server",
-        "description": "Monitor the server state",
-    },
-    {
-        "name": "users",
-        "description": "Operations with users. The **login** logic is also here.",
-    },
-]
 
-app = FastAPI(
-     title="NotaBene API ",
-    openapi_tags=tags_metadata
-)
-security = HTTPBasic()
+# load env 
+from dotenv import load_dotenv
+import os
+load_dotenv()
+# get secret key from env
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
+}
+
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+# oauth2_scheme = HTTPBearer()
+
+app = FastAPI()
+
+# origins = [
+#     "http://localhost.tiangolo.com",
+# ]
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -31,29 +49,61 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# --- Server 
-@app.get("/", tags=["Server"])
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/unixTimes", tags=["Server"])
-async def read_item():
-    unix_timestamp = datetime.now().timestamp()
-    return {"unixTime": unix_timestamp} 
-
-
-# --- User 
-async def get_current_user(credentials: HTTPBasicCredentials = Depends(security)):
-    user = await tasks.authenticate_user(credentials.username, credentials.password)
-    if not user:
-        raise HTTPException(
-            status_code=401,
-            detail="Invalid username or password",
-            headers={"WWW-Authenticate": "Basic"},
-        )
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = tasks.get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
     return user
 
-@app.get("/items/")
-async def read_items(current_user: schemas.User = Depends(get_current_user)):
-    return {"username": current_user.username}
 
+async def get_current_active_user(
+    current_user: Annotated[schemas.User, Depends(get_current_user)]
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+) -> schemas.Token:
+    user = tasks.authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = tasks.create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return schemas.Token(access_token=access_token, token_type="bearer")
+
+
+@app.get("/users/me/", response_model=schemas.User)
+async def read_users_me(
+    current_user: Annotated[schemas.User, Depends(get_current_active_user)]
+):
+    return current_user
+
+
+@app.get("/users/me/items/")
+async def read_own_items(
+    current_user: Annotated[schemas.User, Depends(get_current_active_user)]
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
--- api/tasks.py
+++ api/tasks.py
@@ -1,8 +1,43 @@
-from typing import Optional
+from datetime import datetime, timedelta, timezone
+from passlib.context import CryptContext
+from jose import JWTError, jwt 
 import schemas
+# load env 
+from dotenv import load_dotenv
+import os
+load_dotenv()
+# get secret key from env
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
-async def authenticate_user(username: str, password: str):
-    if username == "u" and password == "p":
-        return schemas.User(username=username, password=password)
-    return None
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return schemas.UserInDB(**user_dict)
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(data: dict, expires_delta: timedelta or None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
